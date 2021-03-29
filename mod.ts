62f4c15042d5aaf8117944b3ca4a464fe8823699
DjDeveloperr <djsandhu2005@gmail.com>
2021-03-29 18:21:36 +0530

    init

---
+++ mod.ts
@@ -0,0 +1,32 @@
+/// <reference path="./runtime.d.ts"/>
+
+import { Client, Interaction, slash, } from 'https://raw.githubusercontent.com/DjDeveloperr/harmony/slash/mod.ts';
+
+class MySlashBot extends Client {
+  constructor() {
+    super({
+      token: Deno.env.get("TOKEN"),
+    })
+
+    this.slash.publicKey = Deno.env.get("PUBLIC_KEY");
+    this.slash.commands.bulkEdit([
+      {
+        name: 'ping',
+        description: 'Just ping!',
+      }
+    ])
+  }
+
+  @slash()
+  run(d: Interaction) {
+    d.reply('Pong!', { flags: 1 << 6 })
+  }
+}
+
+const client = new MySlashBot()
+
+addEventListener("fetch", async (event) => {
+  const d = await client.slash.verifyFetchEvent(event)
+  if (d === false) event.respondWith(new Response(null, { status: 400 }))
+  else client.emit('interactionCreate')
+})
