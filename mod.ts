a31467ec56cd8bc5fa2a1179caf74eb91d2bca37
DjDeveloperr <djsandhu2005@gmail.com>
2021-03-29 18:58:12 +0530

    x

--- mod.ts
+++ mod.ts
@@ -1,4 +1,3 @@
-import "./polyfill.js";
 import { Client, Interaction, InteractionResponseType, InteractionType, slash, } from 'https://raw.githubusercontent.com/DjDeveloperr/harmony/slash/mod.ts';
 
 class MySlashBot extends Client {
--- polyfill.js
+++
@@ -1,9 +0,0 @@
-if (typeof Deno === 'undefined') {
-  window.Deno = {};
-}
-
-if (typeof Deno.errors === 'undefined') {
-  Deno.errors = {
-    UnexpectedEof: Error,
-  };
-}
