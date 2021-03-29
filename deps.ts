3773fb45e457b0641b4ce3ecc2c0e7a01047d7f9
DjDeveloperr <djsandhu2005@gmail.com>
2021-03-29 19:43:13 +0530

    PLEASE WORK

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
+export * from "https://raw.githubusercontent.com/DjDeveloperr/harmony/slash/src/models/client.ts";
 export * from "https://raw.githubusercontent.com/DjDeveloperr/harmony/slash/src/types/slash.ts";
 export * from "https://raw.githubusercontent.com/DjDeveloperr/harmony/slash/src/structures/slash.ts";
-export * from "https://raw.githubusercontent.com/DjDeveloperr/harmony/slash/src/models/client.ts";
