2cd74ec456158434744c34da9da6231f09c16867
DjDeveloperr <djsandhu2005@gmail.com>
2021-03-30 15:27:40 +0530

    'pepega'

--- mod.ts
+++ mod.ts
@@ -1,5 +1,7 @@
 import * as slash from 'https://raw.githubusercontent.com/DjDeveloperr/harmony/slash/deploy.ts'
 
+slash.client.on('interactionError', console.log)
+
 slash.init({ env: true })
 
 slash.handle('ping', (d) => d.reply('Pong!'))
