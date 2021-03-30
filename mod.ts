c84bc1b81e7c1ff7755f8b9b301c07dcea7ee06e
DjDeveloperr <djsandhu2005@gmail.com>
2021-03-30 15:49:24 +0530

    x

--- mod.ts
+++ mod.ts
@@ -2,6 +2,25 @@ import * as slash from 'https://raw.githubusercontent.com/DjDeveloperr/harmony/s
 
 slash.init({ env: true })
 
+slash.commands.bulkEdit([
+  {
+    name: 'ping',
+    description: 'Just pong!',
+  },
+  {
+    name: 'deferred',
+    description: 'Test deferred response.',
+  },
+])
+
 slash.handle('ping', (d) => {
-  d.reply('Pong!')
+  d.reply('Pong!', { ephemeral: true })
+})
+
+slash.handle('deferred', (d) => {
+  d.defer(true).then((d) => {
+    setTimeout(() => {
+      d.reply('Test works!')
+    }, 1000)
+  })
 })
