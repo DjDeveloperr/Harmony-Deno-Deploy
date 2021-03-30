b91adb5ce8a3df67f098639004c20d5e0599158f
DjDeveloperr <djsandhu2005@gmail.com>
2021-03-30 15:25:50 +0530

    x

--- mod.ts
+++ mod.ts
@@ -1,37 +1,5 @@
-import { Client, Interaction, InteractionResponseType, InteractionType, slash, } from './deps.ts';
+import * as slash from 'https://raw.githubusercontent.com/DjDeveloperr/harmony/slash/deploy.ts'
 
-class MySlashBot extends Client {
-  constructor() {
-    super({
-      token: Deno.env.get("TOKEN"),
-    })
+slash.init({ env: true })
 
-    this.slash.publicKey = Deno.env.get("PUBLIC_KEY");
-    // this.slash.commands.bulkEdit([
-    //   {
-    //     name: 'ping',
-    //     description: 'Just ping!',
-    //   }
-    // ])
-  }
-
-  @slash('ping')
-  pingCmd(d: Interaction) {
-    d.reply('Pong!', { flags: 1 << 6 })
-  }
-}
-
-const client = new MySlashBot()
-
-addEventListener("fetch", async (event) => {
-  try {
-    const d = await client.slash.verifyFetchEvent({ request: event.request, respondWith: (...args: any[]) => { event.respondWith(...args); } })
-    if (d === false) event.respondWith(new Response(null, { status: 400 }))
-    else {
-      if (d.type === InteractionType.PING) d.respond({ type: InteractionResponseType.PONG })
-      else client.emit('interactionCreate', d)
-    }
-  } catch (e) {
-    event.respondWith(new Response(e.stack, { status: 500 }))
-  }
-})
+slash.handle('ping', (d) => d.reply('Pong!'))
